b52b17c9d9a12b22e5f3c77eaca6754fdc411e52
syzzzang <syzz1105@naver.com>
2021-02-16 17:39:58 +0900

    update mainpage : KSY

--- GFC/src/gfc/controller/MainController.java
+++ GFC/src/gfc/controller/MainController.java
@@ -1,14 +1,30 @@
 package gfc.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+
+import gfc.dto.Location;
+import gfc.dto.Song;
+import gfc.service.LocationService;
+import gfc.service.SongService;
 
 @Controller
 public class MainController {
+	@Autowired
+	private SongService songService;
 	
+	@Autowired
+	private LocationService locationService;
 	@GetMapping("/main")
-	public String addlocationForm() {
+	public String addlocationForm(Model model) throws Exception{
+		List<Song> songs = songService.getSongList();
+		model.addAttribute("songList", songs);
+		List<Location> locations = locationService.getLocationList();
+		model.addAttribute("location",locations);
 		return "main";
 	}
 }
